06d53c6f67ce1f2a5081a019b7c0de887a6b6b09
jangjeongha <jjha0125@nate.com>
2024-07-10 17:53:51 +0900

    0710

---
+++ Boot10_Restful_API/src/main/java/com/example/boot10/Boot10RestfulApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.boot10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Boot10RestfulApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Boot10RestfulApiApplication.class, args);
+	}
+
+}
